387990986a05aaadd513ec684482e429646bdf53
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-14 01:47:57 +0700

    fix model conversation

--- models/conversation.model.js
+++ models/conversation.model.js
@@ -4,8 +4,8 @@ const TABLE="Conversation";
 module.exports={
     //get list coversation by fanpage_id and sender_id 
     getConversationsByFanpageAndSenderID: function(condition){
-        var result   = db.load(`select * from ${TABLE} where fanpage_id = ? and sender_id = ? `,
-        [condition.fanpage_id,condition.sender_id]);
+        var result   = db.load(`select * from ${TABLE} where fanpage_id = ? and sender_id = ? limit ? offset ?`,
+        [condition.fanpage_id,condition.sender_id,condition.limit,condition.offset]);
         return result;
     },
 
