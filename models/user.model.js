c2b02befc33345e616a5d102b1f6c23b6c00c829
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-13 15:45:15 +0700

    model user

---
+++ models/user.model.js
@@ -0,0 +1,44 @@
+const db = require('../util/db');
+const TABLE="User";
+
+module.exports={
+    //get list user in DB by page
+    getList: function(condition){
+        var result   = db.get(TABLE,condition.limit,condition.offset);
+        return result;
+    },
+
+    //count all user
+    countAll:function(){
+        var result;
+        result  = db.load(`select count(*) as count from ${TABLE} `);
+        return result;
+    },
+
+    
+    //get details user(saler) by ID
+    getOne:function(condition){
+        return db.getOneByCondition(sql,condition);
+    },
+
+    //search list by phone number
+    searchByPhone:function(condition){
+        var sql=`select * from ${TABLE} where phone_number LIKE ? `;
+        return db.load(sql,condition);
+    },
+
+    //add new user 
+    add:function(value){
+        return db.insert(TABLE,value);
+    },
+
+    //update infomation user
+    update:function(condition,value){
+        return db.load(`UPDATE ${TABLE} SET ? WHERE user_id=${condition.user_id}`,value);
+    },
+
+    //delete user
+    delete:function(condition){
+        return db.delete(TABLE,condition);
+    }
+}
