10f33882d6eca23090c7a19247555c80efe4491f
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-15 16:59:59 +0700

    token file - create accesstoken

--- util/token.js
+++ util/token.js
@@ -17,18 +17,47 @@ module.exports={
         const refreshToken = jwt.sign(payload, process.env.TOKEN_SECRET_REFRESHTOKEN,{ expiresIn:"30d" });
 
         //reponse
-        return{
+        return {
             code:20,
             message:"Login success.",
             user:{
                 user_id         :user.user_id,
                 user_name       :user.user_name,
-                phone_number    :user.phone_number,
                 AccessToken     :AccessToken,
                 refreshToken    :refreshToken
             }
 
         };  
+    },
+
+    //get accesstoken 
+    GetAccessToken:async function(user){
+        var payload={
+                user_id             :user.user_id,
+                user_name           :user.user_name,
+                role_type           :user.role_type,
+                status              :user.status,
+                iat                 :Math.floor(Date.now() / 1000),
+        };
+
+        //create AccessToken 
+        const AccessToken = jwt.sign(payload, process.env.TOKEN_SECRET_ACCESSTOKEN,{ expiresIn: "1d"});
+       
+        //reponse
+        return {
+                code:20,
+                message:"get accesstoken success.",
+                user:{
+                    user_id         :user.user_id,
+                    user_name       :user.user_name,
+                    AccessToken     :AccessToken,
+                }
+        };  
+    },
+
+    //verify accesstoken/refreshtoken 
+    verifyToken:async function(token,key){
+        return jwt.verify(token,key);  
     }
 }
   
