6a90ee3ce107f3acb133a9bf462346040a226ad9
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-16 01:07:12 +0700

    util and router for _user

---
+++ routers/_user.router.js
@@ -0,0 +1,29 @@
+const userModel = require("../models/user.model");
+const LINK = require("../util/links.json");
+
+module.exports = {
+    userRouters:function(app){   
+        app.get(    LINK.CLIENT.USER_GET_DETAILS_BY_USER_ID                  ,this.getOneByUserID);
+    },
+    //get details user information
+     getOneByUserID:async function(req,res,next){
+ 
+        var condition={
+            user_id:    req.user.user_id
+        };
+        
+        try{
+            //get result in DB
+            var result= await userModel.getOne(condition);
+
+        }catch(e){
+            console.log(e);
+            return res.status(500).send("server error ");
+        }
+
+        return res.status(200).json({
+            code:20,
+            data:result
+        })
+    },
+}
