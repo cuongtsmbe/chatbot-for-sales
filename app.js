1bf883814db6ffeb74d8bb7e69a34b2224938703
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-16 03:26:15 +0700

    add router and authorize in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 80;
 const bodyParser = require('body-parser');
+const authMdw   = require("./mdw/_auth.mdw");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,12 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+//Authorization middleware
+authMdw.authorize();
+
 //CLIENT
 require("./routers/_authentication.router").authRouters(app);
 require("./routers/_buyer.router").buyerRouters(app);
-require("./routers/_order.router").orderRouters(app);
 require("./routers/_conversation.router").conversationRouters(app);
+require("./routers/_fanpage.router").fanpageRouters(app);
+require("./routers/_order.router").orderRouters(app);
 require("./routers/_prompt.router").promptRouters(app);
+require("./routers/_user.router").userRouters(app);
 //ADMIN
 require("./routers/user.router").userRouters(app);
 require("./routers/fanpage.router").fanpageRouters(app);
