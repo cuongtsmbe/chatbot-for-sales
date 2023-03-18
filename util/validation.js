a4e3a7fc95c39f478551cc9ea620697559e33f95
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-18 18:06:33 +0700

    fix validation for input fanpage

--- util/validation.js
+++ util/validation.js
@@ -11,9 +11,9 @@ module.exports = {
 
     //validate value of fanpage input
     validateFanpageInput:function(value){
-      if (value.status && value.key_fanpage && value.key_open_ai && value.name && value.active != null && value.user_id && value.payment_due_date) {
+      if (value.key_fanpage && value.key_open_ai && value.name && value.active && value.user_id && value.payment_due_date) {
         // Các giá trị không null, undefined hoặc rỗng
-        if (typeof value.active !== 'boolean') {
+        if (String(value.active).toLowerCase() != "true" && String(value.active).toLowerCase() != "false") {
             // active không phải là kiểu boolean
             return 'active phải là kiểu boolean';
         } else if (!/^\d{4}-\d{2}-\d{2}$/.test(value.payment_due_date)) {
