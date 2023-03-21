e5ac91de1f8d11aaf831bdad7cfccf26a125567e
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-21 15:53:44 +0700

    fix db

--- util/db.js
+++ util/db.js
@@ -1,80 +1,83 @@
 const mysql = require('mysql');
 const configMysql = require("../config/mysql");
 
-try{
-    //connect DB
-    const  connection = mysql.createConnection(configMysql.json());
-    connection.connect();
-}catch(e){
-    console.log(e);
-}
+const  pool = mysql.createPool(configMysql.json());
+
 module.exports = {
     load: function(sql,condition) {
         return new Promise(function(resolve, reject) {
-        
-            connection.query(sql,condition, function(error, results, fields) {
-                if (error) {
-                    console.log(error); 
-                    return resolve([]);
-                };
-                resolve(results);
+            pool.getConnection(function(err, connection) {
+                connection.query(sql,condition, function(error, results, fields) {
+                    if (error) {
+                        console.log(error); 
+                        return resolve([]);
+                    };
+                    //giaỉ phóng kết nối
+                    connection.release();
+                    resolve(results);
+                });
             });
-            
         });
     },
     get: function(table,limit,offset) {
         return new Promise(function(resolve, reject) {
-           
-            connection.query(`SELECT * from ${table} LIMIT ? OFFSET ?`,[limit,offset], function(error, results, fields) {
-                if (error) {
-                    console.log(error); 
-                    return resolve([]);
-                };
-                resolve(results);
+            pool.getConnection(function(err, connection) {
+                connection.query(`SELECT * from ${table} LIMIT ? OFFSET ?`,[limit,offset], function(error, results, fields) {
+                    if (error) {
+                        console.log(error); 
+                        return resolve([]);
+                    };
+                    //giaỉ phóng kết nối
+                    connection.release();
+                    resolve(results);
+                });
             });
-
-            
         });
     },
     insert: function(table, data) {
         return new Promise(function(resolve, reject) {
-          
-            connection.query(`INSERT INTO ${table} SET ?`, data, function(error, results, fields) {
-                if (error) {
-                    console.log(error); 
-                    return resolve([]);
-                };
-                resolve(results);
+            pool.getConnection(function(err, connection) {
+                connection.query(`INSERT INTO ${table} SET ?`, data, function(error, results, fields) {
+                    if (error) {
+                        console.log(error); 
+                        return resolve([]);
+                    };
+                    //giaỉ phóng kết nối
+                    connection.release();
+                    resolve(results);
+                });
             });
-
         });
     },
     getOneByCondition: function(table, condition) {
         return new Promise(function(resolve, reject) {
-           
-            connection.query(`select * from ${table} where ?`, condition, function(error, results, fields) {
-                if (error) {
-                    console.log(error); 
-                    return resolve([]);
-                };
-
-                resolve(results);
+            pool.getConnection(function(err, connection) {
+                connection.query(`select * from ${table} where ?`, condition, function(error, results, fields) {
+                    if (error) {
+                        console.log(error); 
+                        return resolve([]);
+                    };
+                    //giaỉ phóng kết nối
+                    connection.release();
+                    resolve(results);
+                });
             });
-
           
         });
     },
     delete: function(table, con) {
         return new Promise(function(resolve, reject) {
-           
-            connection.query(`DELETE FROM ${table} WHERE ?`, con, function(error, results, fields) {
-                if (error) {
-                    console.log(error); 
-                    return resolve([]);
-                };
-                resolve(results);
+            pool.getConnection(function(err, connection) {
+                connection.query(`DELETE FROM ${table} WHERE ?`, con, function(error, results, fields) {
+                    if (error) {
+                        console.log(error); 
+                        return resolve([]);
+                    };
+                    //giaỉ phóng kết nối
+                    connection.release();
+                    resolve(results);
+                });
             });
-
         });
     },
 
