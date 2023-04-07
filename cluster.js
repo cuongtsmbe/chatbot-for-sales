1e5a8c517b9deffa6d6ca06532dbf283f70a6587
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-04-08 04:30:57 +0700

    cluster

---
+++ cluster.js
@@ -0,0 +1,38 @@
+const cluster = require("cluster");
+const http = require("http");
+const numCPUs = require("os").cpus().length;
+const { setupMaster } = require("@socket.io/sticky");
+const { setupPrimary } = require("@socket.io/cluster-adapter");
+const app = require("./app");
+
+
+if (cluster.isMaster) {
+    console.log(`[*Master* ${process.pid}] is running`);
+
+    const httpServer = http.createServer(app);
+
+    setupMaster(httpServer, {
+      loadBalancingMethod: "least-connection",
+    });
+
+    // setup connections between the workers
+    setupPrimary();
+
+    cluster.setupPrimary({
+      serialization: "advanced",
+    });
+
+    httpServer.listen(process.env.PORT);
+
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on("exit", (worker) => {
+      console.log(`Worker ${worker.process.pid} died`);
+      cluster.fork();
+    });
+} else {
+    console.log(`[*Worker* ${process.pid}] started`);
+    require("./websocket/socket");
+}
